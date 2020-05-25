5f586064d13c9f016d4ae5c7da7195082238ea5a
Prateek Mahindra <prateekmahindra9@gmail.com>
2020-05-25 18:17:41 +0400

    Controllers added and small fix to the routes

---
+++ app/controllers/third_party_contents_controller.rb
@@ -0,0 +1,2 @@
+class ThirdPartyContentsController < ApplicationController
+end
---
+++ test/controllers/own_contents_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class OwnContentsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
