6dfaf874059ccc9b81d8842d17e8b267a0dbadd1
Tim Vierboom <tim-vierboom@gmx.de>
2020-06-02 17:49:40 +0200

    Created 1st seed article

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying own_contents..."
+OwnContent.destroy_all
+
+puts "Creating article..."
+article1 = OwnContent.create!({
+  title: '1st article',
+  description: 'this is the description of the first article, i.e. it\'\s short abstract',
+  article: 'This is the actual article content of the first article',
+  category: 'category of first article'
+})
