a87da70822059a480ba6499e5ad02d539316a7a7
Tim Vierboom <tim-vierboom@gmx.de>
2020-06-02 18:14:55 +0200

    change @own-content to @own_contents in index

--- app/controllers/own_contents_controller.rb
+++ app/controllers/own_contents_controller.rb
@@ -1,11 +1,12 @@
 class OwnContentsController < ApplicationController
  before_action :find_content, only: [:show]
   def index
-    @own_content = OwnContent.all
-    @today_content = @own_content.select {|own_content| own_content.published_date == Date.today()}
+    @own_contents = OwnContent.all
+    @today_content = @own_contents.select {|own_content| own_content.published_date == Date.today()}
   end
 
   def show
+    @own_content = Booking.find(params[:id])
   end
 
   private
