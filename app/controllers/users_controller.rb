8750fd11688ede4d0ad00f3bc9786d70bacdfa47
Earl Quinlan earl.quinlan@gmail.com <earl.quinlan@gmail.com>
2020-06-04 12:56:14 +0200

    fix navbar / user dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,13 @@
 class UsersController < ApplicationController
-# include Pundit
-#     skip_after_action :verify_authorized, only: [:home]
-
 #     skip_before_action :authenticate_user!, only: [:home]
-
   def index
-    # @wishes = policy_scope(Wish).where(user: current_user)
-    # @wishes = Wish.all
-    # @users = User.all
-
-    # @email = @users[0].email
   end
 
   def show
     @user = User.find(params[:id])
+
+    @userEmail = User.find(params[:id]).email
+
   end
 
 end
-
