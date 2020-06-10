4338b7bac033fd8bd978ba29a3f3bc4f388193b0
Prateek Mahindra <prateekmahindra9@gmail.com>
2020-06-10 19:21:27 +0400

    Library routes added and revamped routed for newsfeed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,16 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def library
+    @own_contents = OwnContent.all
+    @third_party_contents =ThirdPartyContent.all
+  end
+
+  def newsfeed
+    @own_contents = OwnContent.all
+    @today_content = @own_contents.select {|own_content|  own_content.published_date >= (Date.today()-7) && own_content.published_date <= Date.today() }
+    @week_date = Date.today()-7
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,13 @@ Rails.application.routes.draw do
       put "like" => "own_contents#like"
     end
     resources :comments, only: [:index, :new, :create, :show]
-    resources :favourites, only: [:index, :new, :create, :show]
   end
 
   resources :third_party_contents do
   end
+
+ get '/library', to: 'pages#library'
+ get '/newsfeed', to: 'pages#newsfeed'
 
   resources :users, only: [:index, :show]
       # get "users/:id", to: "users#show"
