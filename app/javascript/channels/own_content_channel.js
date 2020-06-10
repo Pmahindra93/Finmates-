7367bd31bef78cc2fee7eff50420731616d44a7c
Tim Vierboom <tim-vierboom@gmx.de>
2020-06-10 15:09:00 +0200

    force devise user sign_out to GET in routes and devise config

--- app/javascript/channels/own_content_channel.js
+++ app/javascript/channels/own_content_channel.js
@@ -1,17 +1,17 @@
-import cable from '../cable'
+// import consumer from './consumer'
 
-const initOwnContentCable = () => {
-  const ownContentCommentContainer = document.getElementById('own_content_comments');
-  if (ownContentCommentContainer) {
-    const id = ownContentCommentContainer.dataset.chatroomId;
+// const initOwnContentCable = () => {
+//   const ownContentCommentContainer = document.getElementById('own_content_comments');
+//   if (ownContentCommentContainer) {
+//     const id = ownContentCommentContainer.dataset.ownContentId;
 
-    consumer.subscriptions.create({ channel: "OwnContentChannel", id: id }, {
-      received(data) {
-        ownContentCommentContainer.insertAdjacentHTML('beforeend', data);
-        // console.log(data); // called when data is broadcast in the cable
-      },
-    });
-  }
-}
+//     consumer.subscriptions.create({ channel: "OwnContentChannel", id: id }, {
+//       received(data) {
+//         ownContentCommentContainer.insertAdjacentHTML('beforeend', data);
+//         // console.log(data); // called when data is broadcast in the cable
+//       },
+//     });
+//   }
+// }
 
-export { initOwnContentCable };
+// export { initOwnContentCable };
