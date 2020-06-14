28346ad5300041ab556c17069a6b7b68af1e457c
Prateek Mahindra <prateekmahindra9@gmail.com>
2020-06-14 18:55:29 +0400

    Favourites bug fixed

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,4 +1,9 @@
 class Favourite < ApplicationRecord
   belongs_to :own_content
   belongs_to :user
+
+   validates :user_id, uniqueness: {
+    scope: [:own_content_id],
+    message: 'can only favorite an item once'
+  }
 end
