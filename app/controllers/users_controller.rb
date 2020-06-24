2497b59ea480de8cfef3cf114a104a850629375f
Earl Quinlan earl.quinlan@gmail.com <earl.quinlan@gmail.com>
2020-06-24 14:55:42 +0200

    prof-pic update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,32 @@
 class UsersController < ApplicationController
-#     skip_before_action :authenticate_user!, only: [:home]
+    before_action :find_user, only: [:show, :update]
+
   def index
   end
 
   def show
-    @user = User.find(params[:id])
-    authorize @user
-    @userEmail = User.find(params[:id]).email
-    @nickname = User.find(params[:id]).nickname
+    @userEmail = @user.email
+    @nickname = @user.nickname
+    @firstName = @user.first_name
+    @lastName = @user.last_name
   end
 
   def update
-    @avatar = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
+  end
+
+  def edit
   end
 
 private
 
-  def user_params
-    params.require(:user).permit(:avatar)
+  def find_user
+    @user = User.find(params[:id])
+    authorize @user
   end
 
+  def user_params
+    params.require(:user).permit(:avatar, :email, :nickname, :first_name, :last_name)
+  end
 end
