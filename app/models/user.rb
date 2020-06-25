68e26df9b48c53c1b00307bf03a3df1f97dedc8d
Prateek Mahindra <prateekmahindra9@gmail.com>
2020-06-25 18:01:06 +0400

    Few changes to the devise file

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
   devise :omniauthable, omniauth_providers: [:facebook]
 
-  has_many :favourites
+  has_many :favourites, dependent: :destroy
   has_many :own_content_comments, dependent: :destroy
   has_many :own_contents, dependent: :destroy
 
