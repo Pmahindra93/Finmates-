5a2b9cb16b70420577c756c0fb19e280c3447e3e
Tim Vierboom <tim-vierboom@gmx.de>
2020-07-31 13:09:00 +0200

    fb sign up fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   acts_as_voter
 
   validates_uniqueness_of :nickname, :allow_blank => true
-  validates :email, :first_name, :last_name, presence: true
+  # validates :email, :first_name, :last_name, presence: true
 
   # Avatar
   has_one_attached :avatar
