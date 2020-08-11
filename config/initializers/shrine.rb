e6f3e6ffda24b59a7d2c4618c264414231c4cf58
Tim Vierboom <tim-vierboom@gmx.de>
2020-08-11 11:06:34 +0200

    added dev env to shrine

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -15,24 +15,27 @@ require "shrine/storage/file_system"
 require "cloudinary"
 require "shrine/storage/cloudinary"
 
-Cloudinary.config(
-  cloud_name: "dvscdtpyl",
-  api_key:    "581579419931912",
-  api_secret: "GiDIqUEiqumYXmOZXM1xIVMf3AC",
-)
 
-if Rails.env.test?
+if Rails.env.development?
   Shrine.storages = {
     cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
     store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),       # permanent
   }
+
+  Shrine.plugin :activerecord
+  Shrine.plugin :cached_attachment_data # for retaining the cached file across form redisplays
+  Shrine.plugin :restore_cached_data # re-extract metadata when attaching a cached file
+
 else
+  Cloudinary.config(
+    cloud_name: "dvscdtpyl",
+    api_key:    "581579419931912",
+    api_secret: "GiDIqUEiqumYXmOZXM1xIVMf3AC",
+  )
+
   Shrine.storages = {
     cache: Shrine::Storage::Cloudinary.new(prefix: "cache"), # for direct uploads
     store: Shrine::Storage::Cloudinary.new,
   }
 end
 
-Shrine.plugin :activerecord
-Shrine.plugin :cached_attachment_data # for retaining the cached file across form redisplays
-Shrine.plugin :restore_cached_data # re-extract metadata when attaching a cached file
