b00edc530e9c796b4cdab859881dbeb8725e7f88
Prateek Mahindra <prateekmahindra9@gmail.com>
2020-10-06 13:26:16 +0400

    Last three articles will always show in the newsfeed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -21,9 +21,10 @@ class PagesController < ApplicationController
 
   def newsfeed
     @own_contents = OwnContent.all
-    @today_content = @own_contents.select {|own_content|  own_content.published_date >= (Date.today()- 14) && own_content.published_date <= Date.today() }
-    @today_content = @today_content.sort_by {|article| article.published_date}
-    @today_content = @today_content.reverse
+    #@today_content = @own_contents.select {|own_content|  own_content.published_date >= (Date.today()- 14) && own_content.published_date <= Date.today() }
+    #@today_content = @today_content.sort_by {|article| article.published_date}
+    #@today_content = @today_content.reverse
+    @today_content = @own_contents.last(3)
     @week_date = Date.today()-7
     @didyouknows = Didyouknow.all
     @education_contents = EducationContent.all
